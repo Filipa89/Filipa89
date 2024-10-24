e4fb76e34e8a8124e0cc929b1c3e883480f2c777
Filipa89 <filipa.ag.ferreira@gmail.com>
2024-10-24 10:26:51 +0100

    24out10:26

---
+++ 24-10-2024/poo-02-FF.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+extern "C" {
+    #include "./timestamp_FF.h"
+}
+
+class Estudante{
+public:
+    string nome;
+    int idade;
+
+    void mostrar(){
+        cout << nome << " " << idade << endl;
+    }
+};
+
+int main() {
+    Estudante novoEstudante;
+    cin >> novoEstudante.nome >> novoEstudante.idade;
+    novoEstudante.mostrar();
+
+    return 0;
+    meuCarimbo();
+}
