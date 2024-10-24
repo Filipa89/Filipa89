39dfdb7daaf115097b1f3664cd3a2206a96b7d31
Filipa89 <filipa.ag.ferreira@gmail.com>
2024-10-24 09:34:50 +0100

    24out9h34

---
+++ 24-10-2024/poo-01-FF.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+extern "C" {
+    #include "./timestamp_FF.h"
+}
+
+//definição da classe
+class nomeClasse {
+    //declaração de membros privados desta classe
+    private:
+        int valorPrivado;
+
+    //declaração de membros públicos desta classe
+    public:
+        int valorPublico;
+        void funcaoPublica() {
+    }
+}; // Fim da classe 'nomeClasse', termina com ';'
+
+// Programa principal
+int main() { //Inicio do programa principal
+    nomeClasse objetoNovo; //Cria um objeto da classe 'nomeClasse'
+    
+    objetoNovo.valorPublico = 10; //Atribui o valor 10 à variável 'valorPublico'
+
+    objetoNovo.funcaoPublica(); //Também podemos usar as funções públicas definidas dentro da classe
+
+    return 0;
+    
+    meuCarimbo();
+}
+
+
