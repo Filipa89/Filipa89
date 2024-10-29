643a9d3aca03024a0d78ef0a5e2bd258698300ca
Filipa89 <filipa.ag.ferreira@gmail.com>
2024-10-29 12:23:44 +0000

    fim

--- 29-10-2024-FF/poo-06-nv-FF.cpp
+++ 29-10-2024-FF/poo-06-nv-FF.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#include "./timestamp_FF.h"
 
-extern "C" {
-    #include "./timestamp_FF.h"
-}
-
+// Definição da classe Pessoa
 class Pessoa {
+
+// Atributos privados
 private:
     string nome;
     int idade;
     float classificacao;
+    //Atributos privados são frequentemente usados para garantir que o estado interno de um objeto seja protegido e que apenas a classe tenha acesso direto a esses atributos.
 
+// Atributos públicos
 public:
-    // Método para definir os atributos
     void setDados(string nome, int idade, float classificacao) {
         this->nome = nome;
         this->idade = idade;
         this->classificacao = classificacao;
     }
+    //Os métodos públicos são usados para definir a interface da classe, permitindo que os objetos da classe sejam manipulados de forma controlada.
 
     // Método para verificar se pode frequentar o curso
     void podeFrequentar() {
